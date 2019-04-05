63af84435eebcfd640dfa13aeac674fa90d12355
urnotzane <urnotzane@163.com>
2019-04-05 16:01:30 +0800

    自定义tabbaricon完成

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,14 +1,20 @@
 const TABS = [{
     title: '提示组件',
-  url: '/pages/my/index',
+    url: '/pages/my/index',
+    icon: 'icon-menu'
   },
   {
     title: '交互组件',
     url: '/pages/home/index',
+    icon: 'icon-rate'
   }
 ]
 
 Component({
+  options: {
+    addGlobalClass: true,
+  },
+
   data: {
     tabs: TABS,
     active: 0,
@@ -17,11 +23,8 @@ Component({
   methods: {
     handleTabs(e) {
       const item = e.currentTarget.dataset.item
-      const active = e.currentTarget.dataset.index
-      
-      wx.switchTab({
-        url: item.url,
-      })
+      const active = e.currentTarget.dataset.index 
+      wx.switchTab({ url: item.url, })
     }
   },
 })
